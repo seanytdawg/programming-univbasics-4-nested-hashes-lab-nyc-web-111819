b85b132c3f4f95bc1cfe00f1299746588ecbdc78
Sean Tarzy <seantarzy@gmail.com>
2019-11-11 19:56:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,13 +59,14 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ :known_for => "Ruby",
+ :languages => ["LISP", "C"]
+}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -91,7 +92,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
